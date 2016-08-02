da083085d896a715c5f991b455e5d16e05775858
jgm <kayak.jiang@gmail.com>
2016-08-02 21:36:41 +0800

    update

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ActiveRecord::Base
-  mount_uploaders :avatar, AvatarUploader
-
-  attr_accessible :avatar, :avatar_cache
-  
-end
--- db/migrate/20160801145221_add_avatar_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddAvatarToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :avatar, :json
-  end
-end
